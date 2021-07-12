da76173ce4506e61ca156bbb117953e6ca466c8f
Divyanshu Dev Awasthi <66584976+divyanshuhub@users.noreply.github.com>
2021-07-13 01:49:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"],"include_files":['tcl86t.dll','tk86t.dll','opencv_videoio_ffmpeg452_64.dll']}
-
+build_exe_options = {"packages": ["os"],"include_files":['tcl86t.dll','tk86t.dll']}
+#build_exe_options = {"packages": ["os"],"include_files":['tcl86t.dll','tk86t.dll','opencv_videoio_ffmpeg452_64.dll']}
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
